868df5562001e0a02a83c4bf6d5fbac24186e2b0
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-08-27 23:52:47 +0400

    feat: add sample file names

--- src/FileTypeClassifier.java
+++ src/FileTypeClassifier.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileTypeClassifier {
+    public static void main(String[] args) {
+        List<String> FileNames = new ArrayList<>();
+        FileNames.add("report.pdf");
+        FileNames.add("photo.jpg");
+        FileNames.add("program.java");
+        FileNames.add("notes.txt");
+        FileNames.add("image.png");
+        FileNames.add("script.py");
+        FileNames.add("document.docx");
+        FileNames.add("music.mp3");
+        FileNames.add("README");
+
+    }
 }
