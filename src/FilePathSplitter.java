6249345a96f598236d340ce5e8880a8fa34ab757
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-08-28 00:06:44 +0400

    feat: add file path input and validation

--- src/FilePathSplitter.java
+++ src/FilePathSplitter.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class FilePathSplitter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        IO.println("Enter File Path: ");
+        String Path = input.nextLine();
+        if (Path.isBlank()) {
+            IO.println("Invalid path.");
+            return;
+        }
+        input.close();
+    }
 }
