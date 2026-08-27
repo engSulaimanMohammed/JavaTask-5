3e56f8b2326bdd54e104679a18cbe16437b63532
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-08-28 00:05:18 +0400

    feat: Add FilePathSplitter class

---
+++ src/FilePathSplitter.java
@@ -0,0 +1,2 @@
+public class FilePathSplitter {
+}
--- src/FileTypeClassifier.java
+++ src/FileTypeClassifier.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class FileTypeClassifier {
     public static void main(String[] args) {
