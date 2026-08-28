c5b72a4825a116e5469574f5de4189def6ccf8bb
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-08-28 23:48:55 +0400

    feat: add file inventory data and counters

---
+++ src/FileInventoryReport.java
@@ -0,0 +1,15 @@
+public class FileInventoryReport {
+    public static void main(String[] args) {
+        String[] records = {"report.pdf,120", "photo.png,340", "music.mp3,5000", "app.java,8",
+                "data.xlsx,250", "script.py,15", "unknown.xyz,50", "broken-record" };
+
+        int totalSize = 0;
+        int documentCount = 0;
+        int imageCount = 0;
+        int audioCount = 0;
+        int codeCount = 0;
+        int otherCount = 0;
+        String largestFile = "";
+        int largestSize = 0;
+    }
+}
