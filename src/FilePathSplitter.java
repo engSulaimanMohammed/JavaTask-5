0b2037ee957650d41dc2ac73adef02a6e2ee39de
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-08-28 22:20:07 +0400

    feat: detect extension and path style

--- src/FilePathSplitter.java
+++ src/FilePathSplitter.java
@@ -12,9 +12,26 @@ public class FilePathSplitter {
         input.close();
         boolean isWindows = Path.contains("\\");
         Path = Path.replace('\\', '/');
+
         int slashIndex = Path.lastIndexOf('/');
         String folder = Path.substring(0, slashIndex);
         String fileName = Path.substring(slashIndex + 1);
+
+        int dotIndex = fileName.lastIndexOf('.');
+        String extension;
+
+        if (dotIndex != -1) {
+            extension = fileName.substring(dotIndex + 1).toLowerCase();
+        } else {
+            extension = "none";
+        }
+
+        String pathStyle;
+        if (isWindows) {
+            pathStyle = "Windows Path";
+        } else {
+            pathStyle = "Unix Path";
+        }
 
 
     }
