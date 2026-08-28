c9c7e03424ddf85fa0f636ff80ee46a12a0f6805
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-08-28 22:18:18 +0400

    feat: split folder and file name from path

--- src/FilePathSplitter.java
+++ src/FilePathSplitter.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-
 public class FilePathSplitter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         IO.println("Enter File Path: ");
         String Path = input.nextLine();
         if (Path.isBlank()) {
@@ -10,5 +10,12 @@ public class FilePathSplitter {
             return;
         }
         input.close();
+        boolean isWindows = Path.contains("\\");
+        Path = Path.replace('\\', '/');
+        int slashIndex = Path.lastIndexOf('/');
+        String folder = Path.substring(0, slashIndex);
+        String fileName = Path.substring(slashIndex + 1);
+
+
     }
 }
